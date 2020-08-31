89c758ef20eddcd4ede7535e91aafcf111f1014a
Ilija Stevcev <ilija1@gmail.com>
2020-08-31 01:01:15 -0400

    build out unit tests

--- tests/test_logtron_aws.py
+++ tests/test_logtron_aws.py
@@ -1,22 +1,67 @@
+import logging
+
 from logtron_aws import CloudWatchHandler
 from logtron_aws import discover_context
+from logtron_aws import autodiscover
 
 
-def test_cloudwatch():
-    pass
+class MockSTSClientMeta:
+    def __init__(self):
+        self.region_name = "us-east-1"
+
+
+class MockSTSClient:
+    def __init__(self):
+        self.meta = MockSTSClientMeta()
+
+    def get_caller_identity(self):
+        return {"Arn": "arn:aws:sts::123456789012:assumed-role/foo/session1"}
 
 
 def test_context():
-    class MockClientMeta:
-        def __init__(self):
-            self.region_name = "us-east-1"
-
-    class MockClient:
-        def __init__(self):
-            self.meta = MockClientMeta()
-
-        def get_caller_identity(self):
-            return {"Arn": "arn:aws:sts::123456789012:assumed-role/foo/session1"}
-
-    context = discover_context(sts_client=MockClient())
+    context = discover_context(sts_client=MockSTSClient())
     assert context["id"] is not None
+
+
+def test_autodiscover():
+    logger = autodiscover(refresh=True, discover_context=lambda: discover_context(sts_client=MockSTSClient()))
+    logger.info("hello")
+
+
+def test_cloudwatch():
+    class MockLogsPaginator:
+        def __init__(self):
+            pass
+
+        def paginate(self, **kwargs):
+            return []
+
+    class MockLogsClient:
+        def __init__(self):
+            pass
+
+        def get_paginator(self, name):
+            return MockLogsPaginator()
+
+        def create_log_group(self, **kwargs):
+            pass
+
+        def create_log_stream(self, **kwargs):
+            pass
+
+        def put_log_events(self, **kwargs):
+            return {"nextSequenceToken": "123"}
+
+    config = {
+        "handlers": ["logtron_aws.CloudWatchHandler"],
+        "logtron_aws.CloudWatchHandler": {
+            "logs_client": MockLogsClient(),
+            "interval_sec": 30,
+        },
+    }
+    logger = autodiscover(
+        refresh=True, config=config, discover_context=lambda: discover_context(sts_client=MockSTSClient())
+    )
+    logger.info("hello")
+
+    [i.flush() for i in logging.getLogger().handlers]
