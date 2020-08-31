07980b0037305612274eaaa9d7863e3b71307390
Ilija Stevcev <ilija1@gmail.com>
2020-08-31 01:00:43 -0400

    add autodiscover

--- logtron_aws/__init__.py
+++ logtron_aws/__init__.py
@@ -1,4 +1,11 @@
 __version__ = "0.1.0"
 
+import logging
+
 from logtron_aws.cloudwatch import CloudWatchHandler
 from logtron_aws.context import discover_context
+from logtron import autodiscover as autodiscover_base
+
+
+def autodiscover(name=None, level=logging.INFO, config=None, refresh=False, discover_context=discover_context):
+    return autodiscover_base(name=name, level=level, config=config, refresh=refresh, discover_context=discover_context)
