bee4b5a8d9986aceab936f49c463bebf60ae84c3
Ilija Stevcev <ilija1@gmail.com>
2020-08-31 08:03:31 -0400

    add version discovery and bump version

--- logtron_aws/__init__.py
+++ logtron_aws/__init__.py
@@ -1,10 +1,14 @@
-__version__ = "0.1.1"
-
 import logging
+from importlib_metadata import version
 
 from logtron_aws.cloudwatch import CloudWatchHandler
 from logtron_aws.context import discover_context
 from logtron import autodiscover as autodiscover_base
+
+try:
+    __version__ = version(__package__)
+except:
+    pass
 
 
 def autodiscover(name=None, level=logging.INFO, config=None, refresh=False, discover_context=discover_context):
